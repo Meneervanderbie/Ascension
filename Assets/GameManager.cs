bacecb7e00742e24685f0395117fc1c0dda8fd51
Meneervanderbie <abie@deltion.nl>
2017-10-16 10:58:06 +0200

    Ch-ch-ch-changes
    minor

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour {
 
     public GameBoard gameBoard;
     public UIManager uiMan;
-    public int AscendCost = 1000;
+    public int AscendCost = 100;
 
     public int faith;
 
