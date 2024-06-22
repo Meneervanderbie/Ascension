2063e2a569aee2a3c1e69ce5c564d743a05eef61
Drecon84 <alex.vdbie@gmail.com>
2024-06-22 17:58:26 +0200

    Hand positioning
    Got things to an acceptable point here.

--- Assets/Interface/HandManager.cs
+++ Assets/Interface/HandManager.cs
@@ -8,6 +8,11 @@ public class HandManager : MonoBehaviour
     public GameObject cardModel;
 
     public int startingHandSize;
+    public float verticalShift; // Probably need to change this based on screen size?
+
+    public int horizontalShift; // Probably need to change this based on screen size?
+
+    public int tilt;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +21,12 @@ public class HandManager : MonoBehaviour
         for(int i = 1; i < startingHandSize+1; i++){
             // Calculate the angle and offset for the current card. 
             float curPos = i - middle;
-            float shiftBy = curPos / 6;
+            float shiftBy = curPos / horizontalShift;
+            float shiftVertical = Mathf.Abs(curPos) * verticalShift;
             //Vector3 curCardPos = new Vector3(handAnchor.transform.position.x-shiftBy, handAnchor.transform.position.y, handAnchor.transform.position.z);
-            Vector3 curCardPos = new Vector3(-shiftBy, 0, 0);
+            Vector3 curCardPos = new Vector3(-shiftBy, 0, -shiftVertical);
             Quaternion tempQuat = handAnchor.transform.rotation;
-            tempQuat *= Quaternion.Euler(Vector3.down * curPos*5);
+            tempQuat *= Quaternion.Euler(Vector3.down * curPos*tilt);
             // Need to save the cards to a hand array when we actually have that sort of thing set up. 
             //GameObject temp = Instantiate(cardModel, curCardPos, tempQuat, handAnchor.transform);
             GameObject temp = Instantiate(cardModel, handAnchor.transform, false);
